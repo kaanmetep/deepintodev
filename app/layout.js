8457deab63cb61e9eecbf9adae616a7007dcafed
kaanmetep <kaanpmete@gmail.com>
2025-03-17 03:30:45 +0300

    fix default mode as light mode

--- app/layout.js
+++ app/layout.js
@@ -46,7 +46,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en">
       <head>
         {/* Google Analytics */}
         <Script
