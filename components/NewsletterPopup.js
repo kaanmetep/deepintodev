b713ba2fc0a2d12f7cd84374dbdfad4a1e523ed0
kaanmetep <kaanpmete@gmail.com>
2025-05-07 03:13:11 +0300

    fix mobile popup bug

--- components/NewsletterPopup.js
+++ components/NewsletterPopup.js
@@ -23,7 +23,7 @@ export default function NewsletterPopup() {
   if (!showPopup) return null;
 
   return (
-    <div className="fixed bottom-4 right-8 z-50 bg-white dark:bg-gray-800 p-6 rounded-lg shadow-lg w-full max-w-3xl">
+    <div className="fixed bottom-4 right-4 lg:right-8 z-50 bg-white dark:bg-gray-800 p-6 rounded-lg shadow-lg w-full max-w-3xl">
       <button
         onClick={() => setShowPopup(false)}
         className="cursor-pointer absolute top-2 right-4 text-gray-900 hover:text-gray-950 dark:text-gray-200 dark:hover:text-gray-200 text-xl"
